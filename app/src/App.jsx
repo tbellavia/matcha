00b36315d7dcd1eedd6b54237b281f427083f978
bbellavi <pro.bellavi@tutanota.com>
2023-03-06 20:39:57 +0100

    Add: basic app context and configure react router

---
+++ app/src/App.jsx
@@ -0,0 +1,12 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./pages/home";
+
+const router = createBrowserRouter([
+  { path: "/", element: <Home/> }
+]);
+
+function App() {
+  return <RouterProvider router={router}/>
+}
+
+export default App;
