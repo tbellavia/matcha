555548eed4e98411f3b52fa6f5fc11acc73a1f5c
bbellavi <pro.bellavi@tutanota.com>
2023-03-07 15:48:30 +0100

    Add: finish button themes

--- app/src/pages/home.jsx
+++ app/src/pages/home.jsx
@@ -1,13 +1,31 @@
+import { useContext } from "react";
 import Button from "../components/ui/button/Button";
 import style from "./Home.module.css";
+import AppContext from "../store/AppContext";
 
 function Home() {
+    const ctx = useContext(AppContext);
+
+    const onThemeChange = ({target}) => {
+        ctx.setTheme(target.value);
+    }
+
     return (
         <div className={style.home}>
-            <Button variant="regular">inscription</Button>
-            <Button variant="validation">valider</Button>
-            <Button variant="action-regular">nop</Button>
-            <Button variant="action-danger">okay</Button>
+            <select 
+                name="theme"
+                id="theme"
+                className={style.theme}
+                onChange={onThemeChange}
+            >
+                <option value="light">light</option>
+                <option value="dark">dark</option>
+                <option value="blind">blind</option>
+            </select>
+            <Button variant="regular">regular</Button>
+            <Button variant="validation">validation</Button>
+            <Button variant="action-regular">action regular</Button>
+            <Button variant="action-danger">action danger</Button>
         </div>
     );
 }
