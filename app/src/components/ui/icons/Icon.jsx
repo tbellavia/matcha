c32ccdfb21805278ceb5e88fc28898665b65f34c
bbellavi <pro.bellavi@tutanota.com>
2023-03-11 22:01:55 +0100

    Add: icons

---
+++ app/src/components/ui/icons/Icon.jsx
@@ -0,0 +1,25 @@
+import IconError from "./IconError";
+import IconWarning from "./IconWarning";
+import IconInfo from "./IconInfo";
+import IconNotification from "./IconNotification";
+
+
+const icons = {
+    error: IconError,
+    warning: IconWarning,
+    notification: IconNotification,
+    info: IconInfo,
+};
+
+function Icon({ variant, className }) {
+    const CurrentIcon = icons[variant];
+
+    if (!CurrentIcon){
+        throw new Error(`Icon: unknown variant '${variant}`);
+    }
+    return (
+        <CurrentIcon className={className}/>
+    );
+}
+
+export default Icon;
