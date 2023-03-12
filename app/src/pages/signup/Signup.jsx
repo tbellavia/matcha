2bda0cfb4f81efb94a128644fdb9990d0f3d188a
bbellavi <pro.bellavi@tutanota.com>
2023-03-12 01:54:35 +0100

    Add: signup form logic and validation

--- app/src/pages/signup/Signup.jsx
+++ app/src/pages/signup/Signup.jsx
@@ -1,5 +1,69 @@
+import Page from "../page/Page";
+import Background from "../../components/ui/background/Background";
+import Form from "../../components/ui/form/Form";
+import Input from "../../components/ui/input/Input";
+import styles from "./Signup.module.css";
+import { useContext, useEffect, useState } from "react";
+import useDebounce from "../../hooks/use-debounce";
+import { validateEmail, validatePassword } from "../../utils/validation";
+
 function Signup() {
-    return <h1>Signup</h1>;
+    const [email, setEmail] = useState({ value: "", valid: false });
+    const [password, setPassword] = useState({ value: "", valid: false });
+    const [validationPassword, setValidationPassword] = useState({ value: "", valid: false });
+    const [formIsValid, setFormIsValid] = useState(false);
+    const [errorMsg, setErrorMsg] = useState("");
+
+    useEffect(() => {
+        setValidationPassword(prev => {
+            return { 
+                ...prev, 
+                valid: (prev.value == password.value && prev.value.length > 0)
+            };
+        });
+    }, [password.value, validationPassword.value])
+    
+    useEffect(() => {
+        setFormIsValid(email.valid && password.valid && validationPassword.valid);
+    }, [email.valid, password.valid, validationPassword.valid]);
+
+    const emailChange = (value) => {
+        // Check if email doesn't already exists
+        setEmail({ value: value, valid: validateEmail(value) });
+    }
+
+    const passwordChange = (value) => {
+        setPassword({ value: value, valid: validatePassword(value) });
+    }
+
+    const validationPasswordChange = (value) => {
+        setValidationPassword({ value: value, valid: false })
+    }
+
+    const onSubmitHandler = (e) => {
+        e.preventDefault();
+        if (!formIsValid) {
+            console.log("Form is not valid!");
+            return;
+        }
+        console.log("Form is valid!");
+    }
+
+    // let alert;
+
+    // if ()
+
+    return (
+        <Page>
+            <Background paddingTop={2} title="inscription">
+                <Form label="valider" onSubmit={onSubmitHandler}>
+                    <Input label="mail" type="email" value={email.value} onChange={emailChange} />
+                    <Input label="mot de passe" type="password" value={password.value} onChange={passwordChange} />
+                    <Input label="confirmation" type="password" value={validationPassword.value} onChange={validationPasswordChange} />
+                </Form>
+            </Background>
+        </Page>
+    );
 }
 
 export default Signup;
