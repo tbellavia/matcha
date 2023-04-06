e087fe634bb4b82593a1f5475c15314ada46d07b
mvo-van <mainhivvt@gmail.com>
2023-04-06 21:57:43 +0200

    add file for error page

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -8,12 +8,14 @@ import "./palette/blind.css";
 import "./palette/shades.css";
 import "./App.css";
 import Test from "./pages/test/Test";
+import Error404 from "./pages/error404/Error404";
 
 const router = createBrowserRouter([
   { path: "/", element: <Home/> },
   { path: "/login", element: <Login/> },
   { path: "/signup", element: <Signup/> },
-  { path: "/test", element: <Test/> }
+  { path: "/test", element: <Test/> },
+  { path: "/error404", element: <Error404/>}
 ]);
 
 function App() {
---
+++ app/src/pages/error404/Erroe404.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import Background from "../../components/ui/background/Background";
+import Page from "../page/Page";
+import Input from "../../components/ui/input/Input";
+import Form from "../../components/ui/form/Form";
+
+function Error404() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const onMailHandler = (value) => {
+        setEmail(value);
+    }
+
+    const onPasswordHandler = (value) => {
+        setPassword(value);
+    }
+
+    const onSubmitHandler = (e) => {
+        e.preventDefault();
+        console.log(`Login: ${email} Password: ${password}`)
+    }
+
+    return (
+        <Page>
+            <Background paddingTop={2} title="connexion">
+                <Form onSubmit={onSubmitHandler} label="valider">
+                    <Input label="mail" type="email" value={email} onChange={onMailHandler} />
+                    <Input label="mot de passe" type="password" value={password} onChange={onPasswordHandler} />
+                </Form>
+            </Background>
+        </Page>
+    );
+}
+
+export default Error404;
