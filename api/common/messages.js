90f1859d11983034ccafa5e64443cdb0da294c01
Tony BELLAVIA <bbellavi@e1r8p7.clusters.42paris.fr>
2023-04-09 14:36:08 +0200

    Add: auth error messages

--- api/common/messages.js
+++ api/common/messages.js
@@ -1,7 +1,8 @@
 const ERROR_MAIL = "Le mail n'est pas valide";
 const ERROR_PASSWORD = "Le mot de passe doit contenir au moins 6 caractères et doit contenir au moins un caractère spécial suivant '@&$!#?'";
 const ERROR_VALIDATION_PASSWORD = "La confirmation ne correspond pas au mot de passe";
-
+const ERROR_INVALID_LOGIN = "L'email ou le mot de passe est incorrect";
+const ERROR_USER_ALREADY_EXIST = "L'utilisateur existe déjà";
 
 module.exports = {
     ERROR_MAIL,
