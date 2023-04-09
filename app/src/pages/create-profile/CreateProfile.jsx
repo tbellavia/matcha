a2192f0d2076382aea81e7343f2e42a4ae7a0bdc
Tony BELLAVIA <bbellavi@e1r8p7.clusters.42paris.fr>
2023-04-09 16:57:24 +0200

    Init: user-profile branch

---
+++ app/src/pages/create-profile/CreateProfile.jsx
@@ -0,0 +1,12 @@
+import Page from "../page/Page";
+
+function CreateProfile(){
+    return (
+        <Page>
+            <section>
+            </section>
+        </Page>
+    );
+}
+
+export default CreateProfile;
