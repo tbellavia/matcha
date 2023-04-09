65e36e7255e4c07aa2fdcb436fe012f8324c639e
Tony BELLAVIA <bbellavi@e1r8p7.clusters.42paris.fr>
2023-04-09 14:12:56 +0200

    Add: presque finished login

--- app/src/pages/login/Login.jsx
+++ app/src/pages/login/Login.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from "react";
+import React, { useRef, useState , useContext } from "react";
 import Background from "../../components/ui/background/Background";
 import Page from "../page/Page";
 import Input from "../../components/ui/input/Input";
@@ -7,6 +7,16 @@ import Alert from "../../components/ui/alert/Alert";
 import { validateEmail } from "../../utils/validation";
 import { ERROR_MAIL, ERROR_PASSWORD } from "../../utils/messages";
 import useErrorManager from "../../hooks/use-error-manager";
+import axios from "axios";
+import AppContext from "../../store/AppContext";
+import jwt_decode from "jwt-decode";
+import { useNavigate } from "react-router-dom";
+
+function hasCreatedProfile(token) {
+    const decoded = jwt_decode(token);
+
+    return decoded.profile_created;
+}
 
 function Login() {
     const [email, setEmail] = useState("");
@@ -16,6 +26,8 @@ function Login() {
     const formValid = errors.length === 0;
     const emailRef = useRef();
     const passwordRef = useRef();
+    const ctx = useContext(AppContext);
+    const navigate = useNavigate();
 
     const onMailHandler = (value) => {
         setEmail(value);
@@ -42,16 +54,29 @@ function Login() {
         }
     }
 
-    const onSubmitHandler = (e) => {
+    const onSubmitHandler = async (e) => {
         e.preventDefault();
         if (!formValid){
             const [[,ref]] = errors;
             ref.current.focus();
-            console.log("Form is not valid!");
             return;
-        } 
-        console.log(`Form is valid: ${formValid}`);
-        console.log(`Login: ${email} Password: ${password}`)
+        }
+        try {
+            const response = await axios.post("http://localhost:3000/api/user/login", {
+                usermail: email,
+                passWord: password,
+            });
+            const token = response.data.access_token;
+
+            if (hasCreatedProfile(token)) {
+                navigate("/feed");
+            } else {
+                navigate("/profile/create");
+            }
+            ctx.setToken(response.data.access_token);
+        } catch (e) {
+            errManager.addError(e.response.data.message, emailRef);
+        }
     }
 
     let errorAlert;
