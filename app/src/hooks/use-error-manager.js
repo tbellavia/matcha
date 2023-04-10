0d6705b48296faacb490fbffc036dc169538f534
bbellavi <pro.bellavi@tutanota.com>
2023-04-10 22:17:57 +0200

    Add: WIP on signup refactor

--- app/src/hooks/use-error-manager.js
+++ app/src/hooks/use-error-manager.js
@@ -23,11 +23,37 @@ export default function useErrorManager() {
         setErrors(new Map());
     };
 
+    // Function to check if there are any errors
+    const hasErrors = () => {
+        return errors.size > 0;
+    }
+
+    // Function to get first error
+    const getFirstError = () => {
+        const [first] = Array.from(errors);
+
+        if (first)
+            return first[0];
+        return undefined;
+    }
+
+    // Function to focus on first error
+    const focusOnError = () => {
+        const [first] = errors;
+
+        if (first && first[1]) {
+            first[1].current.focus();
+        }
+    }
+
     return {
         errors: Array.from(errors),
         addError,
         removeError,
         clearErrors,
+        hasErrors,
+        getFirstError,
+        focusOnError
     };
 }
 
--- app/src/pages/signup/Signup.jsx
+++ app/src/pages/signup/Signup.jsx
@@ -2,84 +2,67 @@ import Page from "../page/Page";
 import Background from "../../components/ui/background/Background";
 import Form from "../../components/ui/form/Form";
 import Input from "../../components/ui/input/Input";
-import { useEffect, useRef, useState } from "react";
-import { validateEmail, validatePassword } from "../../common/validation";
-import Alert from "../../components/ui/alert/Alert";
-import useInput from "../../hooks/use-input";
-import axios from "axios";
+import { useRef, useState} from "react";
 import { useNavigate } from "react-router-dom";
+import useErrorManager from "../../hooks/use-error-manager";
+import { validateEmail, validatePassword } from "../../common/validation";
 import { ERROR_MAIL, ERROR_PASSWORD, ERROR_VALIDATION_PASSWORD } from "../../common/messages";
+import Alert from "../../components/ui/alert/Alert";
+import axios from "axios";
 
 function Signup() {
-    const [email, setEmailValue, setEmailValid, resetEmail] = useInput("", false);
-    const [password, setPasswordValue, setPasswordValid, resetPassword] = useInput("", false);
-    const [
-        validationPassword,
-        setValidationPasswordValue,
-        setValidationPasswordValid,
-        resetValidationPassword
-    ] = useInput("", false);
-    const [formIsValid, setFormIsValid] = useState(false);
-    const [errorMsg, setErrorMsg] = useState("");
     const emailRef = useRef();
     const passwordRef = useRef();
-    const validationPasswordRef = useRef();
+    const validationRef = useRef();
     const navigate = useNavigate();
-
-
-    useEffect(() => {
-        setFormIsValid(email.valid && password.valid && validationPassword.valid);
-    }, [email.valid, password.valid, validationPassword.valid])
-
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [validation, setValidation] = useState("");
+    const errManager = useErrorManager();
 
     // =================== Email ===================
-
-    const emailChange = (value) => {
-        // TODO: Check if email doesn't already exists in database
-        setEmailValue(value);
+    const onEmailChangeHandler = (value) => {
+        setEmail(value);
     }
 
-    const onEmailValidate = (value) => {
-        // Validate email
-        const isValid = validateEmail(value);
+    const onEmailBlurHandler = (value) => {
+        const emailIsValid = validateEmail(value);
 
-        setEmailValid(isValid);
-        if (!isValid)
-            setErrorMsg(ERROR_MAIL);
-        else
-            setErrorMsg("");
+        if (!emailIsValid) {
+            errManager.addError(ERROR_MAIL, emailRef);
+        } else {
+            errManager.removeError(ERROR_MAIL);
+        }
     }
 
     // =================== Password ===================
-
-    const passwordChange = (value) => {
-        setPasswordValue(value);
+    const onPasswordChangeHandler = (value) => {
+        setPassword(value);
     }
 
-    const onPasswordValidate = (value) => {
-        const isValid = validatePassword(value);
+    const onPasswordBlurHandler = (value) => {
+        const passwordIsValid = validatePassword(value);
 
-        setPasswordValid(isValid);
-        if (!isValid)
-            setErrorMsg(ERROR_PASSWORD);
-        else
-            setErrorMsg("");
+        if (!passwordIsValid) {
+            errManager.addError(ERROR_PASSWORD, passwordRef);
+        } else {
+            errManager.removeError(ERROR_PASSWORD);
+        }
     }
+
 
     // =================== Confirmation ===================
-
-    const validationPasswordChange = (value) => {
-        setValidationPasswordValue(value);
+    const onValidationChangeHandler = (value) => {
+        setValidation(value);
     }
 
-    const onValidationPasswordValidate = (value) => {
-        const isValid = value === password.value && value.length > 0;
+    const onValidationBlurHandler = (value) => {
+        const validationIsValid = (value === password);
 
-        setValidationPasswordValid(isValid);
-        if (!isValid) {
-            setErrorMsg(ERROR_VALIDATION_PASSWORD);
+        if (!validationIsValid) {
+            errManager.addError(ERROR_VALIDATION_PASSWORD, validationRef);
         } else {
-            setErrorMsg("");
+            errManager.removeError(ERROR_VALIDATION_PASSWORD);
         }
     }
 
@@ -87,42 +70,37 @@ function Signup() {
 
     const onSubmitHandler = async (e) => {
         e.preventDefault();
-
-        if (!formIsValid) {
-            console.log("Form is not valid!");
-            if (!email.valid) {
-                emailRef.current.focus();
-                onEmailValidate(email.value);
-            } else if (!password.valid) {
-                passwordRef.current.focus();
-                onPasswordValidate(password.value);
-            } else if (!validationPassword.valid) {
-                onValidationPasswordValidate(validationPassword.value);
-                validationPasswordRef.current.focus();
-            }
+        if (errManager.hasErrors()) {
+            errManager.focusOnError();
             return;
         }
-
+        errManager.clearErrors();
+        console.log(errManager.errors);
         try {
-            const response = await axios.post('http://localhost:3000/api/user/signup', {
-                usermail: email.value,
-                passWord: password.value
+            console.log({
+                email,
+                password,
+                validation
             })
+            const response = await axios.post('http://localhost:3000/api/user/signup', {
+                usermail: email,
+                passWord: password
+            })
+            console.log(response.data);
             navigate("/mailValidation")
         }
         catch (e) {
-            setErrorMsg(e.response.data.message);
+            console.log(e);
+            errManager.addError(e.response.data.message);
         }
     }
 
     let errorAlert;
 
-    if (errorMsg) {
+    if (errManager.hasErrors()) {
         errorAlert = (
-            <Alert>
-                <p>{errorMsg}</p>
-            </Alert>
-        )
+            <Alert>{ errManager.getFirstError() }</Alert>
+        );
     }
 
     return (
@@ -132,26 +110,23 @@ function Signup() {
                     <Input
                         label="mail"
                         type="email"
-                        value={email.value}
-                        onChange={emailChange}
-                        onBlur={onEmailValidate}
                         ref={emailRef}
+                        onChange={onEmailChangeHandler}
+                        onBlur={onEmailBlurHandler}
                     />
                     <Input
                         label="mot de passe"
                         type="password"
-                        value={password.value}
-                        onChange={passwordChange}
-                        onBlur={onPasswordValidate}
                         ref={passwordRef}
+                        onChange={onPasswordChangeHandler}
+                        onBlur={onPasswordBlurHandler}
                     />
                     <Input
                         label="confirmation"
                         type="password"
-                        value={validationPassword.value}
-                        onChange={validationPasswordChange}
-                        onBlur={onValidationPasswordValidate}
-                        ref={validationPasswordRef}
+                        ref={validationRef}
+                        onChange={onValidationChangeHandler}
+                        onBlur={onValidationBlurHandler}
                     />
                     {errorAlert}
                 </Form>
