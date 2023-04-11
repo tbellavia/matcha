fa3bdf9f19900a847bf50721e460829d1647705b
Mai-nhi VO VAN TAO <mvo-van-@e1r9p7.clusters.42paris.fr>
2023-04-11 19:15:34 +0200

    ajout de la verif profile

--- api/common/messages.js
+++ api/common/messages.js
@@ -3,9 +3,19 @@ const ERROR_PASSWORD = "Le mot de passe doit contenir au moins 6 caractères et 
 const ERROR_VALIDATION_PASSWORD = "La confirmation ne correspond pas au mot de passe";
 const ERROR_INVALID_LOGIN = "L'email ou le mot de passe est incorrect";
 const ERROR_USER_ALREADY_EXIST = "L'utilisateur existe déjà";
+const ERROR_NEED_TOKEN = "Session inexistante";
+const ERROR_BAD_TOKEN = "Session expirée"
+const ERROR_PROFILE = "Vous n'avez pas compilé votre profile"
+const ERROR_CHAT = "Cette conversation n'existe pas"
 
 module.exports = {
     ERROR_MAIL,
     ERROR_PASSWORD,
     ERROR_VALIDATION_PASSWORD,
+    ERROR_INVALID_LOGIN,
+    ERROR_USER_ALREADY_EXIST,
+    ERROR_NEED_TOKEN,
+    ERROR_BAD_TOKEN,
+    ERROR_PROFILE,
+    ERROR_CHAT
 }
