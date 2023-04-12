a97da9b6da51de8d12e515677e3681ceffb7fb07
Mai-nhi VO VAN TAO <mvo-van-@e2r13p7.clusters.42paris.fr>
2023-04-12 18:19:20 +0200

    add Carousel et maj addPhoto

--- app/src/components/ui/photo/AddPhoto.jsx
+++ app/src/components/ui/photo/AddPhoto.jsx
@@ -2,27 +2,27 @@ import styles from "./AddPhoto.module.css";
 import style from "./Photo.module.css";
 import { useState } from "react";
 import React from "react";
- 
-function AddPhoto(){
+import Icon from "../icons/Icon";
+
+function AddPhoto() {
     const classes = `${styles.addPhoto}`
     const classe = `${style.medium__photo} ${style.photo}`
     const [selectedFile, setSelectedFile] = useState([])
-    
-    const onChangeHandler = ((e) =>{
+
+    const onChangeHandle = ((e) => {
         console.log(e)
         setSelectedFile([...selectedFile, e.target.files[0]])
         e.target.value = null
-        }
+    }
     )
-    
-    const onClickHandle = ((e) =>{
+
+    const onClickHandle = ((e) => {
         console.log(e.target.getAttribute('data-index'))
-        // console.log(e.target.__reactFiber.index)
         let index = parseInt(e.target.getAttribute('data-index'))
         var array = [...selectedFile]; // make a separate copy of the array
         if (index !== -1) {
-          array.splice(index, 1);
-          setSelectedFile(array);
+            array.splice(index, 1);
+            setSelectedFile(array);
         }
         images.splice(index, 1)
     }
@@ -30,27 +30,28 @@ function AddPhoto(){
 
     let images = []
     let inp = null
-    
-    if(selectedFile.length < 5){
-        inp = <label className={classes}><input  
-        type="file"
-        onChange={onChangeHandler}/>+</label>
-    }
-    
-    
-    for(var i= 0; i < selectedFile.length; i++)
-    {
-        images.push(<img key={i} data-index={i}
-            className={classe}  
-            src = {window.URL.createObjectURL(selectedFile[i])}
-            onClick={onClickHandle}/>)
-        // text.push(<div key={i}>{selectedFile[i].name}</div>)
+
+    if (selectedFile.length < 5) {
+        inp = <label className={classes}>
+            <input
+                type="file"
+                onChange={onChangeHandle} />
+            <Icon className={`${styles.icon}`} variant="addCircle" />
+        </label>
     }
 
-    return (<div className={`${styles.divAdd}`}>
-        <React.Fragment>{images}</React.Fragment>
-        {/* <React.Fragment>{text}</React.Fragment> */}
-        {inp}
+
+    for (var i = 0; i < selectedFile.length; i++) {
+        images.push(<img key={i} data-index={i}
+            className={classe}
+            src={window.URL.createObjectURL(selectedFile[i])}
+            onClick={onClickHandle} />)
+    }
+
+    return (
+        <div className={`${styles.divAdd}`}>
+            <React.Fragment>{images}</React.Fragment>
+            {inp}
 
         </div>
     );
---
+++ app/src/components/ui/photo/Carousel.jsx
@@ -0,0 +1,41 @@
+import styles from "./Carousel.module.css"
+import { useState } from "react";
+import React from "react";
+import Photo from "./Photo";
+
+function Carousel({ tabPhotos }) {
+    const [checkedPhotos, setcheckedPhotos] = useState(0)
+
+    const onChangeHandle = ((e) => {
+        console.log(e.target.id)
+        setcheckedPhotos(parseInt(e.target.id))
+    })
+
+    let inputs = []
+    let inputs2 = []
+    for (var i = 0; i < tabPhotos.length; i++) {
+        inputs.push(
+            <React.Fragment key={i}>
+                <input type="radio" className={`${styles.inputCarousel}`} name="a" id={`${i}`} checked={checkedPhotos === i} onChange={onChangeHandle} />
+                <label className={`${styles.labelCarousel}`} htmlFor={`${i}`} ></label>
+                <div className={`${styles.ci}`}>
+                    <img src={tabPhotos[i]} alt="Snow on leafs" />
+                </div>
+            </React.Fragment>
+        )
+
+        inputs2.push(<Photo data={tabPhotos[i]} size="large" />)
+    }
+
+    return (<div>
+            <div className={`${styles["carousel"]}`}>
+                {inputs}
+            </div>
+            <div className={`${styles["scroll"]}`}>
+                {inputs2}
+            </div>
+        </div>
+    )
+}
+
+export default Carousel;
