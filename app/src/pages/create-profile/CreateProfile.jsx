e19f3b36d403c0cf2724567c7aab52af9ae8cd00
Mai-nhi VO VAN TAO <mvo-van-@e1r2p14.clusters.42paris.fr>
2023-04-14 18:09:46 +0200

    maj create profile

--- app/src/pages/create-profile/CreateProfile.jsx
+++ app/src/pages/create-profile/CreateProfile.jsx
@@ -1,5 +1,12 @@
+import AddPhoto from "../../components/ui/photo/AddPhoto";
 import Page from "../page/Page";
 import styles from "./CreateProfile.module.css";
+import Input from "../../components/ui/input/Input"
+import { useState } from "react";
+import DateInput from "../../components/ui/input/DateInput";
+import RadioButtonGroup from "../../components/ui/radio-button/RadioButtonGroup"
+import CheckboxGroup from "../../components/ui/checkbox/CheckboxGroup";
+import InputTagList from "../../components/ui/tags/InputTagList";
 
 function Box({ children }) {
     return <div className={styles['dummy-box']}>
@@ -8,53 +15,69 @@ function Box({ children }) {
 }
 
 function CreateProfile(){
+    const [name, setName] = useState("")
+    const [prenom, setPrenom] = useState("")
+    const [genre, setGenre] = useState([])
+    const [pref, setPref] = useState([])
+    const [bio, setBio] = useState("")
+    const valuesGenre = ["femme","homme", "non binaire"]
+    const [tags, setTags] = useState([])
+    const suggestTagsList = ["un","deux"]
+    const onChangeName = (e) =>{
+        setName(e)
+    }
+    const onChangePrenom = (e) =>{
+        setPrenom(e)
+    }
+    const onChangeGenre = (e) => {
+        setGenre(e)
+    }
+    const onChangePref = (e) => {
+        console.log(e)
+        setPref(e)
+    }
+    const onChangeBio = (e) => {
+        setBio(e)
+    }
     return (
         <Page className={styles['create-profile']}>
             <section className={styles['create-profile__form']}>
                 {/* Profile picture */}
                 <div className={styles['create-profile__image-container']}>
-                    <Box/>
-                    <Box/>
-                    <Box/>
-                    <Box/>
+                    <AddPhoto/>
                 </div>
 
                 {/* Input group */}
                 <div className={styles['create-profile__input-container']}>
-                    <Box/>
-                    <Box/>
+                    <Input label="prenom" type="text" value={prenom} onChange={onChangePrenom}></Input>
+                    <Input label="nom" type="text" value={name} onChange={onChangeName}></Input>
                 </div>
 
                 {/* Input group */}
                 <div className={styles['create-profile__input-container']}>
-                    <Box/>
-                    <Box/>
+                    <DateInput label="date de naissance"/>
+                    <Input label="lieu" type="text" value="Paris"></Input>
+
                 </div>
 
                 {/* Input group */}
                 <div className={styles['create-profile__input-container']}>
                     <div className={styles['create-profile__input-group']}>
-                        <Box/>
-                        <Box/>
-                        <Box/>
+                        <RadioButtonGroup label="genre" initial="homme" values={valuesGenre} onChange={onChangeGenre}/>
                     </div>
                     <div className={styles['create-profile__input-group']}>
-                        <Box/>
-                        <Box/>
-                        <Box/>
+                        <CheckboxGroup label="preference" values={valuesGenre} onChange={onChangePref}/>
                     </div>
                 </div>
 
                 {/* Tag group */}
                 <div className={styles['create-profile__tags-container']}>
-                    <Box/>
-                    <Box/>
-                    <Box/>
+                    <InputTagList tagList={tags} setTagList={setTags}  suggest={suggestTagsList}/>
                 </div>
 
                 {/* Bio */}
                 <div className={styles['create-profile__bio-container']}>
-                    <Box/>
+                    <Input label="bio" type="text" value={bio} onChange={onChangeBio}/>
                 </div>
 
                 {/* Button */}
