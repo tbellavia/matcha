94403d11661112501aa5504945fddd7ef5694874
bbellavi <pro.bellavi@tutanota.com>
2023-04-17 22:45:16 +0200

    Add: onChange props to AddPhoto component

--- app/src/components/ui/photo/AddPhoto.jsx
+++ app/src/components/ui/photo/AddPhoto.jsx
@@ -4,20 +4,19 @@ import { useState } from "react";
 import React from "react";
 import Icon from "../icons/Icon";
 
-function AddPhoto() {
+function AddPhoto({ onChange }) {
     const classes = `${styles.addPhoto}`
     const classe = `${style.medium__photo} ${style.photo}`
     const [selectedFile, setSelectedFile] = useState([])
 
     const onChangeHandle = ((e) => {
-        console.log(e)
-        setSelectedFile([...selectedFile, e.target.files[0]])
+        const newSelectedFiles = [...selectedFile, e.target.files[0]];
+        onChange(newSelectedFiles);
+        setSelectedFile(newSelectedFiles);
         e.target.value = null
-    }
-    )
+    })
 
     const onClickHandle = ((e) => {
-        console.log(e.target.getAttribute('data-index'))
         let index = parseInt(e.target.getAttribute('data-index'))
         var array = [...selectedFile]; // make a separate copy of the array
         if (index !== -1) {
@@ -25,34 +24,40 @@ function AddPhoto() {
             setSelectedFile(array);
         }
         images.splice(index, 1)
-    }
-    )
+    })
 
     let images = []
     let inp = null
 
     if (selectedFile.length < 5) {
-        inp = <label className={classes}>
-            <input
-                type="file"
-                onChange={onChangeHandle} />
-            <Icon className={`${styles.icon}`} variant="addCircle" />
-        </label>
+        inp = (
+            <label className={classes}>
+                <input
+                    type="file"
+                    onChange={onChangeHandle} />
+                <Icon className={`${styles.icon}`} variant="addCircle" />
+            </label>
+        )
     }
 
 
     for (var i = 0; i < selectedFile.length; i++) {
-        images.push(<img key={i} data-index={i}
-            className={classe}
-            src={window.URL.createObjectURL(selectedFile[i])}
-            onClick={onClickHandle} />)
+        images.push(
+            <img
+                key={i}
+                data-index={i}
+                className={classe}
+                src={window.URL.createObjectURL(selectedFile[i])}
+                alt=""
+                onClick={onClickHandle}
+            />
+        )
     }
 
     return (
         <div className={`${styles.divAdd}`}>
             <React.Fragment>{images}</React.Fragment>
             {inp}
-
         </div>
     );
 }
