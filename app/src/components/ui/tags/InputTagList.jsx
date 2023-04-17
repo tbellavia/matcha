e90dcb8f022b047a42c1dd12354c537574ec744a
bbellavi <pro.bellavi@tutanota.com>
2023-04-17 18:35:57 +0200

    Fix: InputTagList with AutoComplete refacto

--- app/src/components/ui/tags/InputTagList.jsx
+++ app/src/components/ui/tags/InputTagList.jsx
@@ -3,27 +3,29 @@ import styles from "./InputTagList.module.css";
 import Tag from "./Tag";
 import Autocomplete from "./Autocomplete";
 
-function InputTagList({tagList, setTagList, suggest}) {
+function InputTagList({ value, onChange, suggest }) {
     const [suggestedTags, setSuggestedTags] = useState(suggest);
-    // const [tagList, setTagList] = useState(tags);
     const [suggestAlreadyUse, setAlreadyUse] = useState([])
     const [newTag, setNewTag] = useState("");
 
-    const valideValue = value => {
-        console.log(value)
-        if(tagList.indexOf(value) === -1){
-            setTagList([...tagList,value])}   
-        if(suggestedTags.indexOf(value) !== -1){
-            setSuggestedTags(prevSuggestedTags => prevSuggestedTags.filter(tag => tag !== value));
-            setAlreadyUse([...suggestAlreadyUse, value])}
+    const valideValue = val => {
+
+        if (value.indexOf(val) === -1) {
+            onChange([...value, val])
+        }
+        if (suggestedTags.indexOf(val) !== -1) {
+            setSuggestedTags(prevSuggestedTags => prevSuggestedTags.filter(tag => tag !== val));
+            setAlreadyUse([...suggestAlreadyUse, val])
+        }
         setNewTag("")
     }
 
-    const onDeleteHandler = (value) => {
-        setTagList(prevTagList => prevTagList.filter(tag => tag !== value));
-        if(suggestAlreadyUse.indexOf(value) !== -1){
-            setAlreadyUse(prevSuggestedTags => prevSuggestedTags.filter(tag => tag !== value));
-            setSuggestedTags([...suggestedTags, value])}
+    const onDeleteHandler = (val) => {
+        onChange(prevTagList => prevTagList.filter(tag => tag !== val));
+        if (suggestAlreadyUse.indexOf(val) !== -1) {
+            setAlreadyUse(prevSuggestedTags => prevSuggestedTags.filter(tag => tag !== val));
+            setSuggestedTags([...suggestedTags, val])
+        }
     }
 
     return (
@@ -33,9 +35,9 @@ function InputTagList({tagList, setTagList, suggest}) {
             </div>
             <div className={styles["tags-container"]}>
                 <ul className={styles["tags"]}>
-                    {tagList.map(
-                        (label, index) => 
-                            <Tag 
+                    {value.map(
+                        (label, index) =>
+                            <Tag
                                 label={label}
                                 key={index}
                                 onDelete={onDeleteHandler}
@@ -44,7 +46,12 @@ function InputTagList({tagList, setTagList, suggest}) {
                     )}
                 </ul>
             </div>
-            <Autocomplete suggest={suggestedTags} input={newTag} setInput={setNewTag} valideValue={valideValue}/>
+            <Autocomplete
+                suggest={suggestedTags}
+                value={newTag}
+                onChange={setNewTag}
+                onSubmit={valideValue}
+            />
         </div>
     );
 }
