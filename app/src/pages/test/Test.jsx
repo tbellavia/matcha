397b7a9aa86f5158631779d83d3106daec0bea52
bbellavi <pro.bellavi@tutanota.com>
2023-04-17 17:27:59 +0200

    Fix: css sizing in AutoComplete component

--- app/src/pages/test/Test.jsx
+++ app/src/pages/test/Test.jsx
@@ -1,30 +1,66 @@
 import styles from "./Test.module.css";
 import Page from "../page/Page";
-import InputTagList from "../../components/ui/tags/InputTagList";
 import Autocomplete from "../../components/ui/tags/Autocomplete"
 import { useState } from "react";
-import Tags from "../../components/ui/tags/Tags";
 
 function Test(){
-    const [test, setTest] = useState(['test','test1','test2','a','z','e','p3','test3','test13','test23','a3','z3','e3','p']);
-    const [suggest, setSugges] = useState(["one", "two", "three"]);
-    
-    const [input, setInput] = useState("");
-    // const valideValue = (val) =>{
-    //     console.log("valideValue")
-    //     setInput(val)
-    //     console.log("valideValue2")
+    const autocompleteList = ['test','test1','test2','a','z','e','p3','test3','test13','test23','a3','z3','e3','p'];
+    const [choice, setChoice] = useState("");
 
-    // }
+    const onSubmitHandler = (val) =>{
+        console.log(`Submit: ${val}`);
+        setChoice(val)
+    }
 
     return (
-        <Page className={styles.test}>
-            <InputTagList tags={test} suggest={suggest}/>
-            {/* <Autocomplete suggest={test} input={input} setInput={setInput} valideValue={valideValue}/> */}
-            {/* <Autocomplete2 suggestions={test}/> */}
-            {/* <Tags tags={test}/> */}
+        <Page className={styles.test} style={{
+            display: "flex",
+            justiyContent: "center",
+            alignItems: "center",
+        }}>
+            <div style={{
+                width: "45vw",
+                marginTop: "150px"
+            }}>
+                <h1>Google</h1>
+                <Autocomplete 
+                    suggest={autocompleteList} 
+                    value={choice} 
+                    onChange={setChoice} 
+                    onSubmit={onSubmitHandler}
+                />
+            </div>
         </Page>
     );
 }
 
 export default Test;
+
+
+// import styles from "./Test.module.css";
+// import Page from "../page/Page";
+// import { useState } from "react";
+// import CheckboxGroup from "../../components/ui/checkbox/CheckboxGroup";
+// import Header from "../../components/ui/header/Header";
+
+// function Test(){
+//     const [checked, setChecked] = useState({});
+
+//     const onChangeHandler = (values) => {
+//         console.log(values);
+//         setChecked(values);
+//     }
+
+//     return (
+//         <Page className={styles.test}>
+//             <Header/>
+//             <CheckboxGroup 
+//                 label="Genre" 
+//                 values={["Homme", "Femme"]}
+//                 onChange={onChangeHandler}
+//             />
+//         </Page>
+//     );
+// }
+
+// export default Test;
