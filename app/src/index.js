5c05320e0abd3faa23adb2a5bb6972e466cbe3d0
bbellavi <pro.bellavi@tutanota.com>
2023-04-17 18:35:05 +0200

    Add: StrictMode

--- app/src/index.js
+++ app/src/index.js
@@ -6,7 +6,9 @@ import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <AppContextProvider>
-    <App />
-  </AppContextProvider>
+  <React.StrictMode>
+    <AppContextProvider>
+      <App />
+    </AppContextProvider>
+  </React.StrictMode>
 );
