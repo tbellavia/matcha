127579f2eca221c0b1b493dd367a8a5370d2b024
cardiox12 <com.bellavi@tutanota.com>
2023-04-18 19:40:03 +0200

    Add: dark and blind mode in Header component

--- app/src/components/ui/header/Header.jsx
+++ app/src/components/ui/header/Header.jsx
@@ -1,8 +1,13 @@
+import { useContext } from "react";
 import styles from "./Header.module.css";
+import AppContext from "../../../store/AppContext";
 
 function Header() {
+    const { theme } = useContext(AppContext);
+    const themeClass = styles[`header__${theme}`];
+
     return (
-        <header className={styles.header}>
+        <header className={`${styles.header} ${themeClass}`}>
             <div className={styles["header__bar"]}></div>
             <h1>MATCHA</h1>
             <div className={styles["header__bar"]}></div>
