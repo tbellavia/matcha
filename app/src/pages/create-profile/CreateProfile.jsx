bf79c83628d1a827479c85d33e9ec2f26f09294e
bbellavi <pro.bellavi@tutanota.com>
2023-04-18 02:04:19 +0200

    Add: WIP on CreateProfile states reducer

--- app/src/pages/create-profile/CreateProfile.jsx
+++ app/src/pages/create-profile/CreateProfile.jsx
@@ -1,5 +1,7 @@
+import { useReducer } from "react";
 import PageHeader from "../../components/ui/page/PageHeader";
 import styles from "./CreateProfile.module.css";
+import _ from "lodash";
 
 function Box({ children }) {
     return <div className={styles['dummy-box']}>
@@ -7,13 +9,57 @@ function Box({ children }) {
     </div>;
 }
 
+const initialProfileState = {
+    photos: [],
+    firstname: null,
+    lastname: null,
+    birthdate: null,
+    location: null,
+    genre: null,
+    preferences: [],
+    tags: [],
+    biography: null
+};
+
+function createInputReducer(validateFn) {
+    return (state, action) => {
+        switch (action.type) {
+            case "UPDATE":
+                return { ...state, value: action.value };
+            case "VALIDATE":
+                return { ...state, valid: validateFn(state.value) };
+            default:
+                throw new Error(`Unkown action '${action}'`);
+        }
+    }
+}
+
+const photosReducer = createInputReducer(photos => 
+    photos && 
+    photos.length > 1 && 
+    photos.length <= 5
+);
+const firstnameReducer = createInputReducer(firstname => _.isEmpty(firstname));
+const lastnameReducer = createInputReducer(lastname => _.isEmpty(lastname));
+const genreReducer = createInputReducer(genre => _.isEmpty(genre));
+const preferencesReducer = createInputReducer(prefs => _.isEmpty(prefs));
+const tagsReducer = createInputReducer(tags => _.isEmpty(tags));
+// TODO: Check if date is in valid range
+const dateReducer = createInputReducer(date => _.isDate(date));
+// TODO: Make sure location is in France
+const locationReducer = createInputReducer(location => true);
+// TODO: Make sure bio is valid
+const biographyReducer = createInputReducer(bio => true);
+
+
+
 function CreateProfile() {
     return (
         <PageHeader className={styles['create-profile']}>
             <section className={styles['create-profile__form']}>
                 {/* Profile picture */}
                 <div className={styles['create-profile__image-container']}>
-                    <Box /> 
+                    <Box />
                 </div>
 
                 {/* Input group */}
