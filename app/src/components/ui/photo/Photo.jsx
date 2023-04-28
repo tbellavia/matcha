5b7a23df19982186cc8acfc402525578f39d8a5e
cardiox12 <com.bellavi@tutanota.com>
2023-04-28 23:12:29 +0200

    Add: error managment in Photo component

--- app/src/components/ui/photo/Photo.jsx
+++ app/src/components/ui/photo/Photo.jsx
@@ -1,8 +1,14 @@
 import styles from "./Photo.module.css";
 
-function Photo({data, size}){
-    const taille = styles[`${size}__photo`]
-    const classes = `${styles.photo} ${taille}`
+const sizes = ['little', 'medium', 'large'];
+
+function Photo({ data, size = 'medium' }){
+    if (!sizes.includes(size))
+        throw new Error(`Photo: invalid size '${size}'`);
+
+    const taille = styles[`${size}__photo`];
+    const classes = `${styles.photo} ${taille}`;
+
     return (
         <img className={classes} src = {data}/>
     );
