7e50aeb9ac8db9ce6de92a12e6465a45a0d0bb45
cardiox12 <com.bellavi@tutanota.com>
2023-04-28 22:41:25 +0200

    Add: Header variant prop, possible variants are 'left', 'right' and 'center'

--- app/src/components/ui/header/Header.jsx
+++ app/src/components/ui/header/Header.jsx
@@ -2,15 +2,24 @@ import { useContext } from "react";
 import styles from "./Header.module.css";
 import AppContext from "../../../store/AppContext";
 
-function Header() {
+const variants = ['center', 'left', 'right'];
+
+function Header({ variant = "center" }) {
     const { theme } = useContext(AppContext);
     const themeClass = styles[`header__${theme}`];
 
+    if (!variants.includes(variant))
+        throw new Error(`Header: unknown variant ${variant}`);
+
+    const leftLineStyle = styles[`${variant}__left-line`];
+    const rightLineStyle = styles[`${variant}__right-line`];
+    
     return (
         <header className={`${styles.header} ${themeClass}`}>
-            <div className={styles["header__bar"]}></div>
+            <div className={`${styles["header__line"]} ${leftLineStyle}`}>
+            </div>
             <h1>MATCHA</h1>
-            <div className={styles["header__bar"]}></div>
+            <div className={`${styles["header__line"]} ${rightLineStyle}`}></div>
         </header>
     );
 }
