71ec5244e8f5c1d883cbb827f7477838ff1fd50c
cardiox12 <com.bellavi@tutanota.com>
2023-05-02 02:50:57 +0200

    Add: value prop to Header

--- app/src/components/ui/header/Header.jsx
+++ app/src/components/ui/header/Header.jsx
@@ -4,7 +4,7 @@ import AppContext from "../../../store/AppContext";
 
 const variants = ['center', 'left', 'right'];
 
-function Header({ variant = "center" }) {
+function Header({ value = "MATCHA", variant = "center" }) {
     const { theme } = useContext(AppContext);
     const themeClass = styles[`header__${theme}`];
 
@@ -18,7 +18,7 @@ function Header({ variant = "center" }) {
         <header className={`${styles.header} ${themeClass}`}>
             <div className={`${styles["header__line"]} ${leftLineStyle}`}>
             </div>
-            <h1>MATCHA</h1>
+            <h1>{value}</h1>
             <div className={`${styles["header__line"]} ${rightLineStyle}`}></div>
         </header>
     );
