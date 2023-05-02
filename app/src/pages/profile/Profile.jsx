0246f38e4ebb01f2344d20833f83490e0f9704bc
cardiox12 <com.bellavi@tutanota.com>
2023-05-03 01:24:50 +0200

    Add: footer nav and WIP on responsive

--- app/src/pages/profile/Profile.jsx
+++ app/src/pages/profile/Profile.jsx
@@ -12,54 +12,60 @@ import Bio from "../../components/ui/bio/Bio";
 function Profile() {
     return (
         <GenericPage className={styles.profile}>
-            <Header variant="left"/>
+            <Header variant="left" />
 
-                <main className={styles['profile-container']}>
-                    <nav className={styles.nav}>
-                        <div className={styles['nav__left-container']}>
-                            <Photo data={tonyImg} size="medium"/>
-                            <div>
-                                <h2 className={styles['name-label']}>TONY</h2>
-                                <h2 className={styles['name-label']}>LE TOMBEUR DE CES DAMES</h2>
-                            </div>
-                        </div>
-                        <div className={styles['nav__right-container']}>
-                            <IconButton iconColor="#EE8B98" label="FILTRES"/>
-                            <IconButton iconColor="#EE8B98" label="CHAT"/>
-                        </div>
-                    </nav>
-
-                    <div className={styles['profile-main-infos']}>
-                        <div className={styles['profile-main-infos-top']}>
-                            <div className={styles['profile-left-infos']}>
-                                <h2 className={styles['name-label']}>TONY</h2>
-                                <h2 className={styles['name-label']}>LE BEAU GOSSE</h2>
-                            </div>
-
-                            <div className={styles['profile-right-infos']}>
-                                <h3>23 ans</h3>
-                                <h4>100 KM</h4>
-                            </div>
-                        </div>
-                        <div className={styles['profile-main-infos-bottom']}>
-                            <Carousel tabPhotos={[tonyImg, tonyImg, tonyImg, tonyImg, tonyImg]}/>
+            <main className={styles['profile-container']}>
+                <nav className={styles.nav}>
+                    <div className={styles['nav__left-container']}>
+                        <Photo data={tonyImg} size="medium" />
+                        <div>
+                            <h2 className={styles['name-label']}>TONY</h2>
+                            <h2 className={styles['name-label']}>LE TOMBEUR DE CES DAMES</h2>
                         </div>
                     </div>
-
-
-                    <div className={styles['bio-container']}>
-                        <Bio value="je suis tony le grand" disabled/>
+                    <div className={styles['nav__right-container']}>
+                        <IconButton iconColor="#EE8B98" label="FILTRES" />
+                        <IconButton iconColor="#EE8B98" label="CHAT" />
                     </div>
+                </nav>
 
-                    <div className={styles['tags-container']}>
-                        <Tags tags={["beer", "football", "love", "yoga"]}/>
-                    </div>
+                <div className={styles['profile-main-infos']}>
+                    <div className={styles['profile-main-infos-top']}>
+                        <div className={styles['profile-left-infos']}>
+                            <h2 className={styles['name-label']}>TONY</h2>
+                            <h2 className={styles['name-label']}>LE BEAU GOSSE</h2>
+                        </div>
 
-                    <div className={styles['button-container']}>
-                        <Button type="submit" variant="regular" className={styles['button']}>NOP</Button>
-                        <Button type="submit" variant="action-danger" className={styles['button']}>OKAY</Button>
+                        <div className={styles['profile-right-infos']}>
+                            <h3>23 ans</h3>
+                            <h4>100 KM</h4>
+                        </div>
                     </div>
-                </main>
+                    <div className={styles['profile-main-infos-bottom']}>
+                        <Carousel tabPhotos={[tonyImg, tonyImg, tonyImg, tonyImg, tonyImg]} />
+                    </div>
+                </div>
+
+
+                <div className={styles['bio-container']}>
+                    <Bio value="je suis tony le grand" disabled />
+                </div>
+
+                <div className={styles['tags-container']}>
+                    <Tags tags={["beer", "football", "love", "yoga"]} />
+                </div>
+
+                <div className={styles['button-container']}>
+                    <Button type="submit" variant="regular" className={styles['button']}>NOP</Button>
+                    <Button type="submit" variant="action-danger" className={styles['button']}>OKAY</Button>
+                </div>
+
+            </main>
+            <div className={styles['footer-nav']}>
+                <IconButton iconColor="#EE8B98" label="MOI" />
+                <IconButton iconColor="#EE8B98" label="FILTRES" />
+                <IconButton iconColor="#EE8B98" label="CHAT" />
+            </div>
         </GenericPage>
     )
 }
