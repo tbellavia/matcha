e2eb9dd32d8f81fa584a947b5fbf38146509a681
cardiox12 <com.bellavi@tutanota.com>
2023-05-14 18:16:35 +0200

    Add: redirect to home on header title click

--- app/src/components/ui/header/Header.jsx
+++ app/src/components/ui/header/Header.jsx
@@ -1,24 +1,30 @@
 import { useContext } from "react";
 import styles from "./Header.module.css";
 import AppContext from "../../../store/AppContext";
+import { useNavigate } from "react-router-dom";
 
 const variants = ['center', 'left', 'right'];
 
 function Header({ value = "MATCHA", variant = "center" }) {
     const { theme } = useContext(AppContext);
     const themeClass = styles[`header__${theme}`];
+    const navigage = useNavigate();
 
     if (!variants.includes(variant))
         throw new Error(`Header: unknown variant ${variant}`);
 
     const leftLineStyle = styles[`${variant}__left-line`];
     const rightLineStyle = styles[`${variant}__right-line`];
+
+    const onTitleClick = () => {
+        navigage("/");
+    }
     
     return (
         <header className={`${styles.header} ${themeClass}`}>
             <div className={`${styles["header__line"]} ${leftLineStyle}`}>
             </div>
-            <h1>{value}</h1>
+            <h1 onClick={onTitleClick}>{value}</h1>
             <div className={`${styles["header__line"]} ${rightLineStyle}`}></div>
         </header>
     );
