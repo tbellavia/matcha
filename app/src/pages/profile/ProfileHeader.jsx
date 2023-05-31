2acc02fd26084f7bca135ead0aa3a88dea0ff500
cardiox12 <com.bellavi@tutanota.com>
2023-05-31 20:36:32 +0200

    Add: props to ProfileHeader

--- app/src/pages/profile/ProfileHeader.jsx
+++ app/src/pages/profile/ProfileHeader.jsx
@@ -1,13 +1,12 @@
-import React from 'react'
+import React from 'react';
 import styles from "./Profile.module.css";
 import { base64ToFile } from "../../common/utils";
 import AppDroddown from "../../components/ui/drawer-menu/AppDropdown";
 import Photo from "../../components/ui/photo/Photo";
-import maleImg from "../../assets/image/male.jpeg"
 import { useTheme } from '../../hooks/use-theme';
 
 
-const ProfileHeader = () => {
+const ProfileHeader = ({ first_name, last_name, image }) => {
     const theme = useTheme();
     const myNameTopStyle = styles[`name-top__${theme}`];
     const myNameBottomStyle = styles[`name-bottom__${theme}`];
@@ -15,10 +14,10 @@ const ProfileHeader = () => {
     return (
         <nav className={styles.nav}>
             <div className={styles['nav__left-container']}>
-                <Photo data={maleImg} size="medium" />
+                <Photo data={base64ToFile(image)} size="medium" />
                 <div>
-                    <h2 className={`${styles['name-label']} ${myNameTopStyle}`}>tony</h2>
-                    <h2 className={`${styles['name-label']} ${myNameBottomStyle}`}>bellavia</h2>
+                    <h2 className={`${styles['name-label']} ${myNameTopStyle}`}>{first_name}</h2>
+                    <h2 className={`${styles['name-label']} ${myNameBottomStyle}`}>{last_name}</h2>
                 </div>
             </div>
             <div className={styles['nav__right-container']}>
