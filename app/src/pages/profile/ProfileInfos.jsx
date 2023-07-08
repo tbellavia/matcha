13181caba3a4e7e51ed3acee6195f4c9dee1d26e
cardiox12 <com.bellavi@tutanota.com>
2023-07-08 20:19:14 +0200

    fix(ProfileInfos): reformat and fix age float to int

--- app/src/pages/profile/ProfileInfos.jsx
+++ app/src/pages/profile/ProfileInfos.jsx
@@ -1,46 +1,48 @@
 import React from 'react';
-// import styles from "./Profile.module.css";
 import styles from "./ProfileInfos.module.scss";
 import Tags from '../../components/ui/tags/Tags';
 import Bio from '../../components/ui/bio/Bio';
 import Carousel from '../../components/ui/photo/Carousel';
-import { useTheme } from '../../hooks/use-theme';
+import {useTheme} from '../../hooks/use-theme';
 
 
 const ProfileInfos = ({profileInfos}) => {
     const theme = useTheme();
     const navLabelColor = styles[`nav-color__${theme}`];
 
-    return (
-        <React.Fragment>
+    const {
+        tags, biograpy, photo1, distance, photo3, photo2, photo4, photo5, age, last_name, first_name
+    } = profileInfos;
+
+
+    return (<React.Fragment>
             <div className={`${styles['profile-main-infos']} ${navLabelColor}`}>
                 <div className={styles['profile-main-infos-top']}>
                     <div className={styles['profile-left-infos']}>
-                        <h2 className={styles['name-label']}>{profileInfos.first_name}</h2>
-                        <h2 className={styles['name-label']}>{profileInfos.last_name}</h2>
+                        <h2 className={styles['name-label']}>{first_name}</h2>
+                        <h2 className={styles['name-label']}>{last_name}</h2>
                     </div>
 
                     <div className={styles['profile-right-infos']}>
-                        <h3>{profileInfos.age} ans</h3>
-                        <h4>{profileInfos.distance} KM</h4>
+                        <h3>{Math.floor(age)} ans</h3>
+                        <h4>{distance} KM</h4>
                     </div>
                 </div>
                 <div className={styles['profile-main-infos-bottom']}>
-                    <Carousel tabPhotos={[profileInfos.photo1, profileInfos.photo2, profileInfos.photo3, profileInfos.photo4, profileInfos.photo5]} />
+                    <Carousel tabPhotos={[photo1, photo2, photo3, photo4, photo5]}/>
                 </div>
             </div>
 
 
             <div className={styles['bio-container']}>
-                <Bio value={profileInfos.biograpy} disabled />
+                <Bio value={biograpy} disabled/>
             </div>
 
             <div className={styles['tags-container']}>
                 {/* TODO: Remove brackets when back returns array */}
-                <Tags tags={[profileInfos.tags]} />
+                <Tags tags={[tags]}/>
             </div>
-        </React.Fragment>
-    )
+        </React.Fragment>)
 }
 
 export default ProfileInfos
