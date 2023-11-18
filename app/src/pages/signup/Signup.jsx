2058a5ba5c386a964cfd37ed7b109c00386ff109
cardiox12 <com.bellavi@tutanota.com>
2023-11-18 16:30:42 +0100

    chore: add react hook form

--- app/src/pages/signup/Signup.jsx
+++ app/src/pages/signup/Signup.jsx
@@ -1,17 +1,47 @@
 import {Button, Stack, TextField, Typography} from "@mui/material";
 import Centered from "../../components/centered/Centered";
+import {useForm} from "react-hook-form";
 
 export default function Signup() {
-    return (<Centered>
-        <Stack spacing={3}>
-            <Typography variant="h1" fontSize={60}>
-                Inscription
-            </Typography>
+    const {
+        register,
+        watch,
+        handleSubmit,
+        formState: { errors },
+    } = useForm();
 
-            <TextField size="small" label="email" type="email" required/>
-            <TextField size="small" label="mot de passe" type="password" required/>
-            <TextField size="small" label="confirmation du mot de passe" type="password" id="password" required/>
-            <Button type="submit" variant="contained">valider</Button>
-        </Stack>
-    </Centered>)
+    return (
+        <Centered>
+            <Stack spacing={3}>
+                <Typography variant="h1" fontSize={60}>
+                    Inscription
+                </Typography>
+                <form>
+                    <Stack spacing={3}>
+                        <TextField
+                            size="small"
+                            label="email"
+                            type="email"
+                            required
+                            ref={register("email")}
+                        />
+                        <TextField
+                            size="small"
+                            label="mot de passe"
+                            type="password"
+                            required
+                        />
+                        <TextField
+                            size="small"
+                            label="confirmation du mot de passe"
+                            type="password"
+                            id="password"
+                            required
+                        />
+                        <Button type="submit" variant="contained">valider</Button>
+                    </Stack>
+                </form>
+            </Stack>
+        </Centered>
+    )
 }
