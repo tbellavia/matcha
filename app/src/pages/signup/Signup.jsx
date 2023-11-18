48f1e8d145dff868bb9055489e68e1295a87adab
Mai-nhi VO VAN TAO <mvo-van-@bess-f1r3s3.clusters.42paris.fr>
2023-11-18 18:19:20 +0100

    WIP

--- app/src/pages/signup/Signup.jsx
+++ app/src/pages/signup/Signup.jsx
@@ -1,6 +1,7 @@
-import {Button, Stack, TextField, Typography} from "@mui/material";
+import {Alert, Button, Snackbar, Stack, TextField, Typography} from "@mui/material";
 import Centered from "../../components/centered/Centered";
 import {useForm} from "react-hook-form";
+import { validatePassword, validateEmail } from "../../common/validation";
 
 export default function Signup() {
     const {
@@ -10,33 +11,54 @@ export default function Signup() {
         formState: { errors },
     } = useForm();
 
+    const onSubmit = data => {
+        console.log(data)
+    }
+
+    console.log(watch("email"));
+
     return (
         <Centered>
             <Stack spacing={3}>
                 <Typography variant="h1" fontSize={60}>
                     Inscription
                 </Typography>
-                <form>
+                <form onSubmit={handleSubmit(onSubmit)}>
                     <Stack spacing={3}>
                         <TextField
                             size="small"
                             label="email"
                             type="email"
                             required
-                            ref={register("email")}
+                            {...register("email", { 
+                                required: true, 
+                                validate: validateEmail,
+                            })}
                         />
                         <TextField
                             size="small"
                             label="mot de passe"
                             type="password"
                             required
+
+                            {...register("password", { 
+                                required: true, 
+                                validate: validatePassword,
+                                deps: ["confirmation"]
+                            })}
                         />
                         <TextField
                             size="small"
                             label="confirmation du mot de passe"
                             type="password"
-                            id="password"
                             required
+
+                            {...register("confirmation", { 
+                                required: true, 
+                                validate: (value) => {
+                                    return value === watch('password') ? true : "le mot de passe de confirmation ne correspond pas au mot de passe"
+                                },
+                            })}
                         />
                         <Button type="submit" variant="contained">valider</Button>
                     </Stack>
