1b8af03820e94b287eb89d71f7d6a8e2838e9609
cardiox12 <com.bellavi@tutanota.com>
2023-12-22 02:32:15 +0100

    feat(s3): implement a wrapper around aws-sdk for file upload on s3

---
+++ api/s3/s3.js
@@ -0,0 +1,47 @@
+const { S3Client, PutObjectCommand, DeleteObjectCommand } = require("@aws-sdk/client-s3");
+
+// Wrapper around S3Client (see: https://docs.aws.amazon.com/AWSJavaScriptSDK/v3/latest/client/s3/)
+class S3Storage {
+  constructor(accessKey, secretKey, bucketName) {
+    this.client = new S3Client({
+      endpoint: "https://s3.fr-par.scw.cloud",
+      credentials: {
+        accessKeyId: accessKey,
+        secretAccessKey: secretKey,
+      }
+    });
+    this.baseConfig = {
+      ACL: "public-read",
+      // WARNING: don't remove, this allows the free-tier s3 
+      StorageClass: "ONEZONE_IA",
+      Bucket: bucketName,
+    }
+    this.url = `https://${bucketName}.s3.fr-par.scw.cloud`;
+  }
+
+  // Retrieve the S3 url associated to the object with `key`
+  getObjectUrl(key) {
+    return `${this.url}/${key}`;
+  }
+
+  injectBaseConfig(config) {
+    return {
+      ...config,
+      ...this.baseConfig,
+    }
+  }
+
+  // Upload a file to S3 bucket and returns its public url
+  async uploadFile(content) {
+    const imageUUID = crypto.randomUUID();
+    const config = this.injectBaseConfig({
+      Key: imageUUID,
+      Body: content,
+    });
+
+
+    await this.client.send(new PutObjectCommand(config))
+
+    return this.getObjectUrl(imageUUID);
+  }
+}
