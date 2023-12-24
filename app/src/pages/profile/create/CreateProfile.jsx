9d9709793240a2ac04cd938a015930df13b7f1df
cardiox12 <com.bellavi@tutanota.com>
2023-12-24 02:23:02 +0100

    feat(UploadImage): integrate UploadImage and Gender/Preferences within CreateProfile page

--- app/src/pages/profile/create/CreateProfile.jsx
+++ app/src/pages/profile/create/CreateProfile.jsx
@@ -1,75 +1,93 @@
 import React from "react";
 import Header from "../../../components/header/Header";
-import {Autocomplete, Box, Container, Stack, TextField} from "@mui/material";
+import { Autocomplete, Box, Checkbox, Container, FormControlLabel, FormLabel, Radio, RadioGroup, Stack, TextField } from "@mui/material";
 import styles from "./CreateProfile.module.scss";
-import {DesktopDatePicker, LocalizationProvider} from "@mui/x-date-pickers";
-import {AdapterDayjs} from "@mui/x-date-pickers/AdapterDayjs";
-import {cities} from "../../../utils/cities";
-
+import { DesktopDatePicker, LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { cities } from "../../../utils/cities";
+import ImageUpload from "../components/ImageUpload";
+import FormControl from "@mui/material/FormControl";
 
 export default function CreateProfile() {
-    return (
-        <React.Fragment>
-            <Header position="center"/>
-            <Container sx={{
-                height: 300,
-                maxWidth: {
-                    sm: "80%",
-                    md: 635
-                },
-                display: "flex",
-                flexDirection: "column"
-            }}>
-                <Stack gap={3}>
-                    <Stack
-                        direction="row"
-                        width="100%"
-                        justifyContent="space-between"
-                        flexWrap="wrap"
-                        gap={1}
-                    >
-                        <Box className={styles['image-container']}/>
-                        <Box className={styles['image-container']}/>
-                        <Box className={styles['image-container']}/>
-                        <Box className={styles['image-container']}/>
-                        <Box className={styles['image-container']}/>
-                    </Stack>
-
-                    <InputGroup>
-                        <TextField label="Prénom" variant="outlined" size="small" fullWidth/>
-                        <TextField label="Nom" variant="outlined" size="small" fullWidth/>
-                    </InputGroup>
-
-                    <InputGroup>
-                        <LocalizationProvider dateAdapter={AdapterDayjs}>
-                            <DesktopDatePicker
-                                label="Date de naissance"
-                                slotProps={{textField: {size: "small"}}}
-                                sx={{width: "100%"}}
-                            />
-                        </LocalizationProvider>
-                        <Autocomplete
-                            options={Object.keys(cities)}
-                            renderInput={(params) => <TextField {...params} label="Localisation"/>}
-                            size="small"
-                            fullWidth
-                        />
-                    </InputGroup>
-                </Stack>
-            </Container>
-        </React.Fragment>
-    )
-}
-
-function InputGroup({children}) {
-    return (
-        <Stack
-            direction={{sm: "row", xs: "column"}}
+  return (
+    <React.Fragment>
+      <Header position="center" />
+      <Container sx={{
+        height: 300,
+        maxWidth: {
+          sm: "80%",
+          md: 635
+        },
+        display: "flex",
+        flexDirection: "column"
+      }}>
+        <Stack gap={3}>
+          <Stack
+            direction="row"
             width="100%"
             justifyContent="space-between"
-            gap={2}
-        >
-            {children}
+            flexWrap="wrap"
+            gap={1}
+          >
+            <ImageUpload />
+          </Stack>
+
+          <InputGroup>
+            <TextField label="Prénom" variant="outlined" size="small" fullWidth />
+            <TextField label="Nom" variant="outlined" size="small" fullWidth />
+          </InputGroup>
+
+          <InputGroup>
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+              <DesktopDatePicker
+                label="Date de naissance"
+                slotProps={{ textField: { size: "small" } }}
+                sx={{ width: "100%" }}
+              />
+            </LocalizationProvider>
+            <Autocomplete
+              options={Object.keys(cities)}
+              renderInput={(params) => <TextField {...params} label="Localisation" />}
+              size="small"
+              fullWidth
+            />
+          </InputGroup>
+
+          <InputGroup >
+            <FormControl fullWidth>
+              <FormLabel id="gender-choice">Genre</FormLabel>
+              <RadioGroup name="gender-choice">
+                <FormControlLabel value="female" control={<Radio />} label="Femme" />
+                <FormControlLabel value="male" control={<Radio />} label="Homme" />
+                <FormControlLabel value="non-binary" control={<Radio />} label="Non Binaire" />
+              </RadioGroup>
+            </FormControl>
+
+            <FormControl fullWidth>
+              <FormLabel id="preference-choice" >Préférences</FormLabel>
+              <RadioGroup name="preference-choice" >
+                <FormControlLabel value="female" control={<Checkbox />} label="Femme" />
+                <FormControlLabel value="male" control={<Checkbox />} label="Homme" />
+                <FormControlLabel value="non-binary" control={<Checkbox />} label="Non Binaire" />
+              </RadioGroup>
+            </FormControl>
+          </InputGroup>
+
         </Stack>
-    )
+      </Container>
+    </React.Fragment>
+  )
+}
+
+function InputGroup({ children }) {
+  return (
+    <Stack
+      direction={{ sm: "row", xs: "column" }}
+      fullWidth
+      justifyContent="space-between"
+      gap={2}
+    >
+      {children}
+    </Stack>
+  )
 }
