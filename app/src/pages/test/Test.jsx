8d8ffac5566e3a6de42d3c61275e47c870050ec1
cardiox12 <com.bellavi@tutanota.com>
2024-02-17 21:36:26 +0100

    feat: add test page

---
+++ app/src/pages/test/Test.jsx
@@ -0,0 +1,9 @@
+import { FormControl, Autocomplete, Stack, TextField, Chip, FormLabel } from "@mui/material";
+
+export default function Test() {
+  return (
+    <Stack spacing={3} sx={{ width: 500 }}>
+    </Stack>
+  )
+}
+
