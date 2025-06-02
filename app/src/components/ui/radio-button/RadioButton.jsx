afc0ee0019ee93270eccf9ecf37068956e6b8a4d
fullife32 <eithan.assouline6@gmail.com>
2025-06-02 18:25:36 +0200

    add onChange radioButton

--- app/src/components/ui/radio-button/RadioButton.jsx
+++ app/src/components/ui/radio-button/RadioButton.jsx
@@ -2,13 +2,17 @@ import { useContext } from "react";
 import styles from "./RadioButton.module.css";
 import AppContext from "../../../store/AppContext";
 
-function RadioButton({ name, value, checked = false }) {
+function RadioButton({ name, value, checked = false, onChange = () => {} }) {
     const { theme } = useContext(AppContext);
     const themeStyle = styles[`radio__${theme}`];
 
+    const onChangeHandler = (event) => {
+        onChange(event.target.value);
+    }
+
     return (
         <div className={`${styles.radio} ${themeStyle}`}>
-            <input id={value} type="radio" name={name} value={value} checked={checked}/>
+            <input id={value} type="radio" name={name} value={value} checked={checked} onChange={onChangeHandler}/>
             <label htmlFor={value}>{ value }</label>
         </div>
     );
