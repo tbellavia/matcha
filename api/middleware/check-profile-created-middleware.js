abac459ec7d7ec49f9a25b63c29486f52062575e
Mai-nhi VO VAN TAO <mvo-van-@f6r7s16.clusters.42paris.fr>
2025-07-02 17:53:03 +0200

    fix middleware

--- api/middleware/check-profile-created-middleware.js
+++ api/middleware/check-profile-created-middleware.js
@@ -2,7 +2,7 @@ const checkProfileCreatedMiddleware = (req, res, next) => {
     if (res.locals.profile_created) {
         return next()
     }
-    res.status(401).json({ message: ERROR_PROFILE })
+    res.status(308).json({ message: ERROR_PROFILE })
 }
 
 const checkProfileNotCreatedMiddleware = (req, res, next) => {
--- app/src/hooks/use-fetch.js
+++ app/src/hooks/use-fetch.js
@@ -2,7 +2,7 @@ import axios from "axios"
 import { useNavigate } from "react-router-dom"
 import { useContext } from "react";
 import AppContext from "../store/AppContext";
-import { ERROR_BAD_TOKEN, ERROR_NEED_TOKEN } from "../common/messages";
+import { ERROR_BAD_TOKEN, ERROR_NEED_TOKEN, ERROR_PROFILE } from "../common/messages";
 
 const baseURL = "http://localhost:3000";
 export default function useFetch(unauthorizedFallback = "/login"){
@@ -22,11 +22,18 @@ export default function useFetch(unauthorizedFallback = "/login"){
             });
         }
         catch (e){
+            console.log("here")
             if(e.response && e.response.status === 401){
+                console.log("here1")
                 return navigate(unauthorizedFallback);
             }
             if(e.response && (e.response.data.message === ERROR_BAD_TOKEN  || e.response.data.message === ERROR_NEED_TOKEN)){
+                console.log("here2", e.response.data.message)
                 return navigate(unauthorizedFallback);
+            }
+            if(e.response && (e.response.data.message === ERROR_PROFILE)){
+                console.log("here3")
+                return navigate("profile/create");
             }
         }
     }
