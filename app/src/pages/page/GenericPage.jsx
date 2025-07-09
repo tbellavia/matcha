2eadf5f573551ee9d080e18032e11d382fabb89c
fullife32 <eithan.assouline6@gmail.com>
2025-07-09 18:03:33 +0200

    add footer and fix position in chat

--- app/src/pages/page/GenericPage.jsx
+++ app/src/pages/page/GenericPage.jsx
@@ -1,7 +1,14 @@
+import { useContext } from "react";
+import styles from "./GenericPage.module.css"
+import AppContext from "../../store/AppContext";
+
 function GenericPage({ children, className, style }) {
+    const ctx = useContext(AppContext)
+
     return (
         <div className={className} style={style}>
             {children}
+        <div className={`${styles[`footer__${ctx.theme}`]} ${styles.footer}`}>Copyright © 2020 - 2025 Matcha, Inc. - All Rights Reserved.</div>
         </div>
     );
 }
