b0a544f7fc86ad092d2f8096720e3f2db9eb5e7f
fullife32 <eithan.assouline6@gmail.com>
2025-07-12 13:19:08 +0200

    fix tags label colors

--- app/src/components/ui/tags/Tags.jsx
+++ app/src/components/ui/tags/Tags.jsx
@@ -1,30 +1,28 @@
 import styles from "./InputTagList.module.css";
 import Tag from "./Tag";
+import { useContext } from "react";
+import AppContext from "../../../store/AppContext";
 
-function Tags({tags}) {
-    if (!tags) {
-        tags = [];
-    }
-    return (
-        <div className={styles["input-tag-list"]}>
-            <div className={styles["tag-label-container"]}>
-            <div className={styles.labelDiv}>
-                Tags
-            </div>
-            </div>
-            <div className={styles["tags-container"]}>
-                <ul className={styles["tags"]}>
-                    {tags.map(
-                        (label, index) => 
-                            <Tag 
-                                label={label}
-                                key={index}
-                            />
-                    )}
-                </ul>
-            </div>
-        </div>
-    );
+function Tags({ tags }) {
+	if (!tags) {
+		tags = [];
+	}
+	const { theme } = useContext(AppContext);
+	const labelColor = styles[`labelDiv__${theme}`];
+	return (
+		<div className={styles["input-tag-list"]}>
+			<div className={styles["tag-label-container"]}>
+				<div className={`${labelColor} ${styles.labelDiv}`}>Tags</div>
+			</div>
+			<div className={styles["tags-container"]}>
+				<ul className={styles["tags"]}>
+					{tags.map((label, index) => (
+						<Tag label={label} key={index} />
+					))}
+				</ul>
+			</div>
+		</div>
+	);
 }
 
 export default Tags;
